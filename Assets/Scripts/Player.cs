ff531c68021b4d9c5093d5ef96e28f6ac51a8545
Le Minh Nam <namlmhe163473@fpt.edu.vn>
2024-03-06 16:09:33 +0700

    Character Move with animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,29 +12,33 @@ public class Player : MonoBehaviour
     public Transform BulletSpawnPoint;
     public GameObject BulletPrefab;
     public static UnityAction<float> MinusHealth;
+    public Animator John;
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         MinusHealth += SetMinusHealth;
+        rb = GetComponent<Rigidbody2D>();
+        John = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) // Up
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) // Up
         {
             transform.position += Vector3.up * MovementSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.A)) // Left
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) // Left
         {
             transform.position += Vector3.left * MovementSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.S)) // Down
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) // Down
         {
             transform.position += Vector3.down * MovementSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.D)) // Right
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) // Right
         {
             transform.position += Vector3.right * MovementSpeed * Time.deltaTime;
         }
@@ -45,6 +49,9 @@ public class Player : MonoBehaviour
             Shoot();
         }
         //BulletSpawnPoint.transform.rotation = Quaternion.Euler(0f, 0f, GetAngle());
+
+        //Set animation
+        setAnimationState();
     }
 
     float GetAngle()
@@ -73,5 +80,28 @@ public class Player : MonoBehaviour
         {
             // ending game
         }
+    }
+
+    private void setAnimationState()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            John.SetBool("isWalkUp", true);
+        else
+            John.SetBool("isWalkUp", false);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            John.SetBool("isWalkLeft", true);
+        else
+            John.SetBool("isWalkLeft", false);
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            John.SetBool("isWalkDown", true);
+        else
+            John.SetBool("isWalkDown", false);
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            John.SetBool("isWalkRight", true);
+        else
+            John.SetBool("isWalkRight", false);
     }
 }
