d6eb0288e17000b345ecabfbd8230f2c4fcf88d0
RyuuyaS <thinhyolo1994@gmail.com>
2024-03-12 15:11:41 +0700

    Update Angle

--- Assets/Scripts/WeaponTestScene/Guns/M4A1.cs
+++ Assets/Scripts/WeaponTestScene/Guns/M4A1.cs
@@ -5,9 +5,11 @@ public class M4A1 : BaseRangeWeapon
 {
     public override bool Shoot()
     {
-        if (!base.Shoot()) return false;
-        Instantiate(Bullet, transform.position, Bullet.transform.rotation).GetComponent<BaseBullet>().Direction =
-            GetAngleVector();
+        if (!base.Shoot())
+            return false;
+        Instantiate(Bullet, transform.position, Bullet.transform.rotation)
+            .GetComponent<BaseBullet>()
+            .Direction = GetVectorBaseRotateAngle(0);
         return true;
     }
 }
