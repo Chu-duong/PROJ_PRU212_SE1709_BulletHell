6cc0840f4330efb9cd1b268b1efd872af784248a
Hungvd08 <hungvdhe161733@fpt.edu.vn>
2024-03-19 22:04:56 +0700

    up date

--- Assets/Scripts/Enemy/script/FOLLOW.cs
+++ Assets/Scripts/Enemy/script/FOLLOW.cs
@@ -1,51 +1,11 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using Unity.Mathematics;
-//using UnityEngine;
-
-//public class FOLLOW : MonoBehaviour
-//{
-//    public GameObject player;
-//    public float speed;
-//    public bool flip;
-
-//    Rigidbody2D rb;
-
-//    private void Awake()
-//    {
-//        rb = GetComponent<Rigidbody2D>();
-//    }
-
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        Vector3 scale = transform.localScale;
-//        if (player.transform.position.x > transform.position.x)
-//        {
-//            scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1) ;
-//            transform.Translate(x : speed * Time.deltaTime, y : 0, z : 0);
-//        } else
-//        {
-//            scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1) * -1;
-//            transform.Translate(x: speed * Time.deltaTime * -1, y: 0, z: 0);
-//        }
-//        transform.localScale = scale;
-//    }
-//}
+﻿
 
 
 using UnityEngine;
 
 public class FOLLOW : MonoBehaviour
 {
-    public Transform player;
+    Transform player;
     public float speed;
     public int maxHealth = 3;
     public int currentHealth;
@@ -58,16 +18,14 @@ public class FOLLOW : MonoBehaviour
 
     Rigidbody2D rb;
 
-    private void Awake()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-    }
+    
 
     private void Start()
     {
         currentHealth = maxHealth;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     public void TakeDamage(int damageAmount)
@@ -102,7 +60,5 @@ public class FOLLOW : MonoBehaviour
             anim.SetFloat("di", Mathf.Abs(direction.x));
         }
             
-
-
     }
 }
