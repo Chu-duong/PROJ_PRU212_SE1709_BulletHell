0fce6edc03ed320489ead1246e43073f33758e25
Bui Tien Dung <dungbt2202.forwork@gmail.com>
2024-03-20 13:24:49 +0700

    add map and take dmg effect

--- Assets/Scripts/Enemy/script/CommonEnemy.cs
+++ Assets/Scripts/Enemy/script/CommonEnemy.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public abstract class CommonEnemy : MonoBehaviour
 {
     public static UnityAction KillAction;
     public int currentHealth;
+    private float rValue = 1f;
 
     public void TakeDamage(int damageAmount)
     {
@@ -14,6 +16,9 @@ public abstract class CommonEnemy : MonoBehaviour
         {
             Die();
         }
+
+        // For effect
+        rValue = 0f;
     }
 
     public void Die()
@@ -23,6 +28,15 @@ public abstract class CommonEnemy : MonoBehaviour
         Destroy(gameObject);
     }
 
-    
+    void FixedUpdate()
+    {
+        if (rValue < 1f)
+        {
+            rValue += 0.05f;
+            GetComponent<SpriteRenderer>().color = new Color(1.0f, rValue, rValue);
+        }
+    }
+
+
 
 }
